326c56830e73eb39ae53b19ff832e9026427e602
Korn Sooksatra <kornsook@Korns-Air.lan>
2021-03-19 07:30:07 -0500

    dfsd

--- server.py
+++ server.py
@@ -7,11 +7,14 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            for i in range(100000):
-                conn.sendall(b'Hi')
+    while(True):
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                for i in range(100000):
+                    conn.sendall(b'Hi')
