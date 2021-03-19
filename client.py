90691df2f61ca5d9c6145d67203fb2752ee72a11
Korn Sooksatra <kornsook@Korns-MacBook-Air.local>
2021-03-19 12:50:40 -0500

    dfsd

--- client.py
+++ client.py
@@ -13,31 +13,30 @@ def multi_threaded_client(host,port):
     global done
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host,port))
-        for i in range(20):
-            s.sendall(b'Hello, world')
-            sum = 0
-            while(sum < expected_size):
-                data = s.recv(1024)
-                sum+=len(data)
+        s.sendall(b'Hello, world')
+        sum = 0
+        while(sum < expected_size):
+            data = s.recv(1024)
+            sum+=len(data)
         done += 1
 for i in range(host_start,host_end+1):
     if(i in host_except):
         continue
-    num_except = 0
-    done = 0
+    num_except = 0    
     for j in host_except:
         if(j <= i):
             num_except += 1
     N = i - host_start + 1 - num_except
     print('N = {}'.format(N))
     start = time.time()
-    for j in range(host_start,i+1):
-        if(j in host_except):
+    for k in range(20):
+        done = 0
+        for j in range(host_start,i+1):
+            if(j in host_except):
+                continue
+            host = '192.168.2.' + str(j)
+            start_new_thread(multi_threaded_client,(host,PORT))
+        while(done != N):
             continue
-        host = '192.168.2.' + str(j)
-        print(host)
-        start_new_thread(multi_threaded_client,(host,PORT))
-    while(done != N):
-        continue
     end = time.time()
-    print('Latency: {}\n Goodput: {}'.format(end-start, 4*N/(end-start)))
+    print('Latency: {}\nGoodput: {}'.format(end-start, 4*N/(end-start)))
--- setBufferSize.sh
+++ setBufferSize.sh
@@ -1,5 +1,10 @@
+#!\bin\bash
+
+if [ $1 -eq 1 ]
+then
 sudo tc qdisc del dev $1 root
-sudo tc qdisc add dev $1 root handle 1: htb default 10
-sudo tc class add dev $1 parent 1: classid 1:10 htb rate 1000Mbit
-sudo tc qdisc add dev $1 parent 1:10 handle 20: netem delay 1ms limit $2
+fi
+sudo tc qdisc add dev $2 root handle 1: htb default 10
+sudo tc class add dev $2 parent 1: classid 1:10 htb rate $3Mbit
+sudo tc qdisc add dev $2 parent 1:10 handle 20: netem delay 1ms limit $4
 
