d73aad8709e1421bac696b24b77762851b83c26f
Korn Sooksatra <kornsook@Korns-MacBook-Air.local>
2021-03-19 11:21:11 -0500

    dfsd

--- client.py
+++ client.py
@@ -3,13 +3,16 @@
 import socket, time
 from _thread import *
 
-HOST_LIST = ['192.168.2.2']  # The server's hostname or IP address
+host_start = 2
+host_end = 19
+host_except = [5]
 PORT = 65432        # The port used by the server
 expected_size = 200000
 done = 0
 def multi_threaded_client(host,port):
     global done
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print(host)
         s.connect((host,port))
         for i in range(20):
             s.sendall(b'Hello, world')
@@ -19,9 +22,13 @@ def multi_threaded_client(host,port):
                 sum+=len(data)
         done += 1
 start = time.time()
-for host in HOST_LIST:    
-    multi_threaded_client(host,PORT)
-while(done != len(HOST_LIST)):
+for i in range(host_start,host_end+1):
+    if(i in host_except):
+        continue
+    host = '192.168.2.' + str(i)
+    print(host)
+    start_new_thread(multi_threaded_client,(host,PORT))
+while(done != host_end-host_start+1 - len(host_except)):
     continue
 end = time.time()
 print(end-start)
